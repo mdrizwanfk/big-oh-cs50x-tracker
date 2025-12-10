0e214531217ecee855fcb3f33e8ddadb88c7c186
mdrizwanfk <rizwanfardeenkhan@outlook.com>
2025-12-10 23:55:06 +0530

    chore: Remove deprecated archive files and update App component with new logo and simplified content

--- archive/service-worker.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      alert("Big Oh - A Timer/Stopwatch for CS50");
-    },
-  });
-});
